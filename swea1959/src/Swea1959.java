50ff4e24b0bdca0fc5f1b3a779162835c5970629
bpv834 <bpv8342Gmail.com>
2025-04-30 19:16:00 +0900

    1959. 두 개의 숫자열 윈도우 슬라이딩 처음엔 n m 에서 작은수를 m 에서 설정하고 배열을 받았다 의도 n은 긴배열, m은 짧은배열 하지만 그러면 안되고 배열 받고 짧은 배열과 긴배열에 할당해줘야 함

---
+++ swea1959/src/Swea1959.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Swea1959 {
+}
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[m];
+            int[] lArr;
+            int[] sArr;
+
+
+            for(int i=0; i<n; i++){
+                arr1[i] = sc.nextInt();
+            }
+
+            for(int i=0; i<m; i++){
+                arr2[i] = sc.nextInt();
+            }
+
+            if(n>m){
+                lArr = arr1;
+                sArr = arr2;
+            }else{
+                lArr = arr2;
+                sArr = arr1;
+            }
+
+
+
+
+
+            int dx = 0;
+            int max = -1;
+            while(dx<= lArr.length - sArr.length){
+                int pt = 0;
+                for(int i =0; i<sArr.length; i++){
+                    pt += sArr[i]*lArr[i+dx];
+                }
+                max = Math.max(max,pt);
+                ++dx;
+            }
+            System.out.println("#"+test_case+" "+max);
+
+
+        }
+    }
+}
