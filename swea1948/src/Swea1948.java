d93a54da8c980d088f29e883abc1fc6520cc450f
bpv834 <bpv8342Gmail.com>
2025-05-02 20:09:51 +0900

    1954. 달팽이 숫자 우하좌상 회전하면서 달팽이 채우기 버전 업데이트

---
+++ swea1948/src/Swea1948.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Swea1948 {
+}
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int month = sc.nextInt();
+            int day = sc.nextInt();
+
+            int addMonth = sc.nextInt();
+            int addDays = sc.nextInt();
+
+            int ans = 0;
+
+            HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+            for(int k=1; k<=7;k++){
+                if(k%2 !=0) map.put(k,31);
+                else map.put(k,30);
+            }
+            for(int k=8; k<=12;k++){
+                if(k%2 ==0) map.put(k,31);
+                else map.put(k,30);
+            }
+            map.put(2,28);
+            // 월에 끝 날짜 세팅완료
+
+            if(month == addMonth) ans = addDays - day +1;
+            else{
+                // 1달 차이면
+                if(addMonth - month == 1){
+                    int theDays = map.get(month) - day +1 ;
+                    ans = theDays + addDays;
+                }
+
+                else{
+                    // 1달 차이가 아니면
+                    // 만약 5월~7월이라면 5월에만 며칠인지 구함
+                    int theDays = map.get(month) - day +1 ;
+                    int otherDays = 0;
+                    for(int j=month+1; j<addMonth ; j++){
+                        otherDays+= map.get(j);
+                    }
+                    ans  = theDays+otherDays+addDays ;
+                }
+            }
+            System.out.println("#"+test_case+" "+ans);
+        }
+    }
+}
