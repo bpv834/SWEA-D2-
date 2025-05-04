382f6871f9f32621e956d78772b406873a6abf24
bpv834 <bpv8342Gmail.com>
2025-05-04 15:10:55 +0900

    1928. Base64 Decoder 메모 : 2진수 문자열을 정수로 변환하기 6자리로 포맷하기 정수를 이진수 문자열로 변환하기 Base64 라이브러리 추가

--- swea1928/src/Swea1928.java
+++ swea1928/src/Swea1928.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -91,6 +94,21 @@ class Solution
             }
             System.out.println("#"+test_case+" "+ans);
 
+        }
+    }
+}
+
+class Solution2
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int testCase=1; testCase<=T; testCase++) {
+            String str = br.readLine();
+            String result = new String(Base64.getDecoder().decode(str));
+            System.out.println("#" + testCase + " " + result);
         }
     }
 }
